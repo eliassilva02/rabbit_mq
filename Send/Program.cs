0c907670d6f816564b5aca4401b2f787143df788
Elias Silva <eduardoelias14@hotmail.com>
2024-03-03 17:27:44 -0300

    init

---
+++ Send/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var fact = new ConnectionFactory() { HostName = "localhost" };
+
+using var conn = fact.CreateConnection();
+using var channel = conn.CreateModel();
+
+channel.QueueDeclare(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+const string msg = "Hello World!";
+
+var body = Encoding.UTF8.GetBytes(msg);
+
+channel.BasicPublish(exchange: string.Empty,
+                    routingKey: "hello",
+                    basicProperties: null,
+                    body: body);
+Console.WriteLine($" [x] Sent {msg}");
+
+Console.WriteLine(" Press X to exit.");
